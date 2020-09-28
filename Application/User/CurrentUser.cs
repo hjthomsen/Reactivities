ad4f52125a75245235a9baa44077d2516bfbd24c
Hans Jacob Thomsen <ext_hjt@ft.fo>
2020-09-28 13:18:21 +0100

    End of Section 16

--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Interfaces;
@@ -21,12 +22,12 @@ namespace Application.User {
             }
 
             public async Task<User> Handle (Query request, CancellationToken cancellationToken) {
-                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
-                return new User{
+                var user = await _userManager.FindByNameAsync (_userAccessor.GetCurrentUsername ());
+                return new User {
                     DisplayName = user.DisplayName,
-                    Username = user.UserName,
-                    Token = _jwtGenerator.CreateToken(user),
-                    Image = null
+                        Username = user.UserName,
+                        Token = _jwtGenerator.CreateToken (user),
+                        Image = user.Photos.FirstOrDefault (x => x.IsMain)?.Url
                 };
             }
         }
